4e0f2316581b74dc41051db2c364d30c0be1db60
Erolinda Kajtazi <erolindakajtazi@gmail.com>
2024-05-20 14:20:02 +0200

    adding the challeges

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,16 +3,16 @@ import { useState , useRef} from "react";
 export default function Player() {
   const playerName = useRef();
 
-  //const [enterdPlayerName, setEnterdPlayerName] = useState("");
+  const [enterdPlayerName, setEnterdPlayerName] = useState("");
 
   function handleClick() {
-    //setEnterdPlayerName(playerName.current.value);
+    setEnterdPlayerName(playerName.current.value);
     playerName.current.value = ''; // clear the input // imperative
   }
 
   return (
     <section id="player">
-      <h2>Welcome {playerName.current.value ?? "unknown entity"}</h2>
+      <h2>Welcome {enterdPlayerName ?? "unknown entity"}</h2>
       <p>
         <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
