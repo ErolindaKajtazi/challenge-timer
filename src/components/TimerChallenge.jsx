18f4d5c6ab88d191b732e1e3a7251d5561270448
Erolinda Kajtazi <erolindakajtazi@gmail.com>
2024-05-20 14:55:47 +0200

    managing stop and start challenge

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,18 +1,38 @@
-export default function TimerChallenge({title, targetTime}) {
-    return <section className="challenge">
-        <h2>{title}</h2>
-        <p className="challenge-time">
-            {targetTime} seconds{targetTime > 1 ? 's' : ''}
+import { useState } from "react";
 
-        </p>
-        <p>
-            <button>
-                Start Challenge
-            </button>
-        </p>
-        <p className="">
-            Time is running... / Timer inactive
-        </p>
+export default function TimerChallenge({ title, targetTime }) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
 
+  let timer;
+
+  function handleStart() {
+    timer = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerStarted(true)
+  }
+
+  function handleStop() {
+    clearTimeout(timer);
+  }
+
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
+      <p className="challenge-time">
+        {targetTime} seconds{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
+      </p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
+      </p>
     </section>
+  );
 }
